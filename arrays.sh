f7954d7d1883ffaf4170bef8126d98939c0e4ac5
aperez14 <35715550+aperez14@users.noreply.github.com>
2023-12-17 19:21:16 -0800

    Create arrays.sh

---
+++ arrays.sh
@@ -0,0 +1,40 @@
+my_array=("value 1" "value 2" "value 3" "value 4")
+
+echo ${my_array[1]}
+
+echo ${my_array[-1]}
+
+echo ${my_array[@]}
+
+echo ${#my_array[@]}
+
+#!/bin/bash
+
+letters=( "A""B""C""D""E" ) 
+echo ${letters[@]}
+
+$ ABCDE
+
+#!/bin/bash
+
+letters=( "A""B""C""D""E" ) 
+b=${letters:0:2}
+echo "${b}"
+
+$ AB
+
+#!/bin/bash
+
+letters=( "A""B""C""D""E" ) 
+b=${letters::5}
+echo "${b}"
+
+$ ABCDE
+
+#!/bin/bash
+
+letters=( "A""B""C""D""E" ) 
+b=${letters:3}
+echo "${b}"
+
+$ DE
