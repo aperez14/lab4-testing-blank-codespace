b8a3433c9dbaaa43be4abb130bbb6e825dd7d372
aperez14 <35715550+aperez14@users.noreply.github.com>
2023-12-17 19:17:58 -0800

    comments.sh

---
+++ comments.sh
@@ -0,0 +1,11 @@
+# This is a comment and will not be rendered on the screen
+
+#!/bin/bash
+
+# Ask the user for their name
+
+read -p "What is your name? " name
+
+# Greet the user
+echo "Hi there $name"
+echo "Welcome to DevDojo!"
