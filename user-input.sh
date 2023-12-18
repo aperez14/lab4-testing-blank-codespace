e9848d579eda807aa2bfccd624efc759ccf0b5a2
aperez14 <35715550+aperez14@users.noreply.github.com>
2023-12-17 19:14:45 -0800

    user-input.sh

---
+++ user-input.sh
@@ -0,0 +1,31 @@
+```bash
+#!/bin/bash
+
+echo "What is your name?"
+read name
+
+echo "Hi there $name"
+echo "Welcome to DevDojo!"
+
+```bash
+./devdojo.sh
+```
+
+```
+What is your name?
+Bobby
+```
+
+```
+Hi there Bobby
+Welcome to DevDojo!
+```
+
+```bash
+#!/bin/bash
+
+read -p "What is your name? " name
+
+echo "Hi there $name"
+echo "Welcome to DevDojo!"
+```
